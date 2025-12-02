12e21be410df61df541d1a8a105f32145cfa337c
Fayz Al jabali <alexeyb231@gmail.com>
2025-12-02 06:17:22 +0000

    أانا لا اريد وصف للاصلاحات عليك وبشكل دائمًا وفي كل مرة أن أُنشئ وأُرفق

--- next.config.ts
+++ next.config.ts
@@ -21,12 +21,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
